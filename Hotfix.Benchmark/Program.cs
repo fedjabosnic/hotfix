32a457eecb876476bfdd16eee3f90370c65f253b
Fedja Bosnic <fedjabosnic@gmail.com>
2017-06-07 09:53:04 +0100

    Added benchmark project
    - Added basic benchmark suite

---
+++ Hotfix.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using Hotfix.Benchmark.Suites;
+
+namespace Hotfix.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Basic>();
+        }
+    }
+}
---
+++ Hotfix.Benchmark/Suites/Basic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Engines;
+
+namespace Hotfix.Benchmark.Suites
+{
+    [MemoryDiagnoser]
+    [AllStatisticsColumn]
+    [SimpleJob(RunStrategy.Throughput, launchCount: 1, warmupCount: 5, targetCount: 10, invocationCount: 100000000)]
+    public class Basic
+    {
+        private byte[] Bytes { get; set; }
+
+        [Setup]
+        public void Setup()
+        {
+            Bytes = new byte[1024000];
+        }
+
+        [Benchmark]
+        public string Noop() => string.Empty;
+
+        [Benchmark]
+        public byte IndexingFirstItem() => Bytes[0];
+
+        [Benchmark]
+        public byte IndexingMiddleItem() => Bytes[512000];
+
+        [Benchmark]
+        public byte IndexingLastItem() => Bytes[1024000 - 1];
+    }
+}
