65d7ab5b2992b4b4832d31c7b3e852444f434da4
Fedja Bosnic <fedjabosnic@gmail.com>
2017-06-23 17:50:34 +0100

    Fixed bug in fix message
    - We now use the fields directly when checking length and checksum (since valid has not yet been set to true)

--- HotFix/Core/FIXMessage.cs
+++ HotFix/Core/FIXMessage.cs
@@ -58,8 +58,8 @@ namespace HotFix.Core
                 if (Fields[2].Tag != 35) throw new Exception("MsgType field not found at expected position");
                 if (Fields[Count - 1].Tag != 10) throw new Exception("CheckSum field not found at expected position");
 
-                if (this[9].AsInt != length - this[8].Length - this[9].Length - this[10].Length) throw new Exception("BodyLength of the message does not match");
-                if (this[10].AsInt != (checksum - this[10].Checksum) % 256) throw new Exception("CheckSum of the message does not match");
+                if (Fields[1].AsInt != length - Fields[0].Length - Fields[1].Length - Fields[Count - 1].Length) throw new Exception("BodyLength of the message does not match");
+                if (Fields[Count - 1].AsInt != (checksum - Fields[Count - 1].Checksum) % 256) throw new Exception("CheckSum of the message does not match");
 
                 Valid = true;
             }
