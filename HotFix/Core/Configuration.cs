1f66ba5eb6d84f54c307a2ae3dad4e64ca192d9c
Fedja Bosnic <fedjabosnic@gmail.com>
2017-07-02 22:08:15 +0100

    Removed redundant configuration fields

--- HotFix/Core/Configuration.cs
+++ HotFix/Core/Configuration.cs
@@ -6,20 +6,16 @@ namespace HotFix.Core
     {
         public Role Role { get; set; }
 
-        public int HeartbeatInterval { get; set; }
-
         public string Version { get; set; } = "FIX.4.2";
-
         public string Target { get; set; }
         public string Sender { get; set; }
 
         public string Host { get; set; }
         public int Port { get; set; }
-        
+
+        public int HeartbeatInterval { get; set; }
+
         public long InboundSeqNum { get; set; }
         public long OutboundSeqNum { get; set; }
-
-        public int InboundBufferSize { get; set; }
-        public int OutboundBufferSize { get; set; }
     }
 }
--- HotFix/Core/IConfiguration.cs
+++ HotFix/Core/IConfiguration.cs
@@ -6,20 +6,15 @@ namespace HotFix.Core
     {
         Role Role { get; set; }
 
-        int HeartbeatInterval { get; set; }
-
         string Version { get; set; }
-
         string Target { get; set; }
         string Sender { get; set; }
 
         string Host { get; set; }
         int Port { get; set; }
 
+        int HeartbeatInterval { get; set; }
         long InboundSeqNum { get; set; }
         long OutboundSeqNum { get; set; }
-
-        int InboundBufferSize { get; set; }
-        int OutboundBufferSize { get; set; }
     }
 }
