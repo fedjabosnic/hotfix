1e748e5fcb3baee9f2a7b86ccb2f2272bb51f645
George Flourentzos <georgeflourentzos@gmail.com>
2017-08-14 13:21:01 +0100

    Restructured and renamed the is active at and next closing time extension method tests

--- HotFix.Test/utilities/scheduling/next_closing_time.cs
+++ HotFix.Test/utilities/scheduling/next_closing_time.cs
@@ -10,226 +10,250 @@ namespace HotFix.Test.utilities.scheduling
     [TestClass]
     public class next_closing_time
     {
-        [TestMethod]
-        public void test1()
+        [TestClass]
+        public class open_day_and_time_is_before_close_day_and_time
         {
-            // | M | T | W | T | F | S | S |
-            // | |-------X-------|         |
-
-            new Schedule
+            [TestClass]
+            public class different_open_and_close_days
             {
-                OpenDay = Monday,
-                OpenTime = TimeSpan.Parse("10:00:00"),
+                [TestMethod]
+                public void datetime_inside_session()
+                {
+                    // | M | T | W | T | F | S | S |
+                    // | |-------X-------|         |
 
-                CloseDay = Friday,
-                CloseTime = TimeSpan.Parse("10:00:00")
+                    new Schedule
+                    {
+                        OpenDay = Monday,
+                        OpenTime = TimeSpan.Parse("10:00:00"),
+
+                        CloseDay = Friday,
+                        CloseTime = TimeSpan.Parse("10:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) // Wednesday
+                    .Should().Be(DateTime.ParseExact("30/06/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
+
+                [TestMethod]
+                public void datetime_outside_session()
+                {
+                    // | M | T | W | T | F | S | S |
+                    // | |---------------|   X     |
+
+                    new Schedule
+                    {
+                        OpenDay = Monday,
+                        OpenTime = TimeSpan.Parse("10:00:00"),
+
+                        CloseDay = Friday,
+                        CloseTime = TimeSpan.Parse("10:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("01/07/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) // Saturday
+                    .Should().Be(DateTime.ParseExact("07/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
             }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) // Wednesday
-            .Should().Be(DateTime.ParseExact("30/06/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+
+            [TestClass]
+            public class same_open_and_close_days
+            {
+                [TestMethod]
+                public void datetime_between_open_and_close_times()
+                {
+                    // - 10:00:00
+                    // |
+                    // |
+                    // X 12:00:00
+                    // |
+                    // |
+                    // - 14:00:00
+
+                    new Schedule
+                    {
+                        OpenDay = Wednesday,
+                        OpenTime = TimeSpan.Parse("10:00:00"),
+
+                        CloseDay = Wednesday,
+                        CloseTime = TimeSpan.Parse("14:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
+                    .Should().Be(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
+
+                [TestMethod]
+                public void datetime_after_open_and_close_times()
+                {
+                    // - 10:00:00
+                    // |
+                    // |
+                    // - 12:00:00
+                    // 
+                    // 
+                    // X 14:00:00
+
+                    new Schedule
+                    {
+                        OpenDay = Wednesday,
+                        OpenTime = TimeSpan.Parse("10:00:00"),
+
+                        CloseDay = Wednesday,
+                        CloseTime = TimeSpan.Parse("12:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
+                    .Should().Be(DateTime.ParseExact("05/07/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
+
+                [TestMethod]
+                public void datetime_before_open_and_close_times()
+                {
+                    // X 10:00:00
+                    // 
+                    // 
+                    // - 12:00:00
+                    // |
+                    // |
+                    // - 14:00:00
+
+                    new Schedule
+                    {
+                        OpenDay = Wednesday,
+                        OpenTime = TimeSpan.Parse("12:00:00"),
+
+                        CloseDay = Wednesday,
+                        CloseTime = TimeSpan.Parse("14:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
+                    .Should().Be(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
+            }
         }
 
-        [TestMethod]
-        public void test2()
+        [TestClass]
+        public class open_day_and_time_is_after_close_day_and_time
         {
-            // | M | T | W | T | F | S | S |
-            // | |---------------|   X     |
-
-            new Schedule
+            [TestClass]
+            public class different_open_and_close_days
             {
-                OpenDay = Monday,
-                OpenTime = TimeSpan.Parse("10:00:00"),
+                [TestMethod]
+                public void datetime_inside_session()
+                {
+                    // | M | T | W | T | F | S | S |
+                    // |-|               |---X-----|
 
-                CloseDay = Friday,
-                CloseTime = TimeSpan.Parse("10:00:00")
+                    new Schedule
+                    {
+                        OpenDay = Friday,
+                        OpenTime = TimeSpan.Parse("10:00:00"),
+
+                        CloseDay = Monday,
+                        CloseTime = TimeSpan.Parse("10:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("01/07/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Saturday
+                    .Should().Be(DateTime.ParseExact("03/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
+
+                [TestMethod]
+                public void datetime_outside_session()
+                {
+                    // | M | T | W | T | F | S | S |
+                    // |-|       X       |---------|
+
+                    new Schedule
+                    {
+                        OpenDay = Friday,
+                        OpenTime = TimeSpan.Parse("10:00:00"),
+
+                        CloseDay = Monday,
+                        CloseTime = TimeSpan.Parse("10:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
+                    .Should().Be(DateTime.ParseExact("03/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
             }
-            .NextClosingTime(DateTime.ParseExact("01/07/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) // Saturday
-            .Should().Be(DateTime.ParseExact("07/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
 
-        [TestMethod]
-        public void test3()
-        {
-            // | M | T | W | T | F | S | S |
-            // |-|               |---X-----|
-
-            new Schedule
+            [TestClass]
+            public class same_open_and_close_days
             {
-                OpenDay = Friday,
-                OpenTime = TimeSpan.Parse("10:00:00"),
+                [TestMethod]
+                public void datetime_between_open_and_close_times()
+                {
+                    // |
+                    // |
+                    // - 10:00:00
+                    // 
+                    // 
+                    // X 12:00:00
+                    // 
+                    // 
+                    // - 14:00:00
+                    // |
+                    // |
 
-                CloseDay = Monday,
-                CloseTime = TimeSpan.Parse("10:00:00")
+                    new Schedule
+                    {
+                        OpenDay = Wednesday,
+                        OpenTime = TimeSpan.Parse("14:00:00"),
+
+                        CloseDay = Wednesday,
+                        CloseTime = TimeSpan.Parse("10:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
+                    .Should().Be(DateTime.ParseExact("05/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
+
+                [TestMethod]
+                public void datetime_before_open_and_close_times()
+                {
+                    // |
+                    // |
+                    // X 10:00:00
+                    // |
+                    // |
+                    // - 12:00:00
+                    // 
+                    // 
+                    // - 14:00:00
+                    // |
+                    // |
+
+                    new Schedule
+                    {
+                        OpenDay = Wednesday,
+                        OpenTime = TimeSpan.Parse("14:00:00"),
+
+                        CloseDay = Wednesday,
+                        CloseTime = TimeSpan.Parse("12:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
+                    .Should().Be(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
+
+                [TestMethod]
+                public void datetime_after_open_and_close_times()
+                {
+                    // |
+                    // |
+                    // - 10:00:00
+                    // 
+                    // 
+                    // - 12:00:00
+                    // |
+                    // |
+                    // X 14:00:00
+                    // |
+                    // |
+
+                    new Schedule
+                    {
+                        OpenDay = Wednesday,
+                        OpenTime = TimeSpan.Parse("12:00:00"),
+
+                        CloseDay = Wednesday,
+                        CloseTime = TimeSpan.Parse("10:00:00")
+                    }
+                    .NextClosingTime(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
+                    .Should().Be(DateTime.ParseExact("05/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
+                }
             }
-            .NextClosingTime(DateTime.ParseExact("01/07/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Saturday
-            .Should().Be(DateTime.ParseExact("03/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
-
-        [TestMethod]
-        public void test4()
-        {
-            // | M | T | W | T | F | S | S |
-            // |-|       X       |---------|
-
-            new Schedule
-            {
-                OpenDay = Friday,
-                OpenTime = TimeSpan.Parse("10:00:00"),
-
-                CloseDay = Monday,
-                CloseTime = TimeSpan.Parse("10:00:00")
-            }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
-            .Should().Be(DateTime.ParseExact("03/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
-
-        [TestMethod]
-        public void test5()
-        {
-            // - 10:00:00
-            // |
-            // |
-            // X 12:00:00
-            // |
-            // |
-            // - 14:00:00
-
-            new Schedule
-            {
-                OpenDay = Wednesday,
-                OpenTime = TimeSpan.Parse("10:00:00"),
-
-                CloseDay = Wednesday,
-                CloseTime = TimeSpan.Parse("14:00:00")
-            }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
-            .Should().Be(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
-
-        [TestMethod]
-        public void test6()
-        {
-            // - 10:00:00
-            // |
-            // |
-            // - 12:00:00
-            // 
-            // 
-            // X 14:00:00
-
-            new Schedule
-            {
-                OpenDay = Wednesday,
-                OpenTime = TimeSpan.Parse("10:00:00"),
-
-                CloseDay = Wednesday,
-                CloseTime = TimeSpan.Parse("12:00:00")
-            }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
-            .Should().Be(DateTime.ParseExact("05/07/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
-
-        [TestMethod]
-        public void test7()
-        {
-            // X 10:00:00
-            // 
-            // 
-            // - 12:00:00
-            // |
-            // |
-            // - 14:00:00
-
-            new Schedule
-            {
-                OpenDay = Wednesday,
-                OpenTime = TimeSpan.Parse("12:00:00"),
-
-                CloseDay = Wednesday,
-                CloseTime = TimeSpan.Parse("14:00:00")
-            }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
-            .Should().Be(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
-
-        [TestMethod]
-        public void test8()
-        {
-            // |
-            // |
-            // - 10:00:00
-            // 
-            // 
-            // X 12:00:00
-            // 
-            // 
-            // - 14:00:00
-            // |
-            // |
-
-            new Schedule
-            {
-                OpenDay = Wednesday,
-                OpenTime = TimeSpan.Parse("14:00:00"),
-
-                CloseDay = Wednesday,
-                CloseTime = TimeSpan.Parse("10:00:00")
-            }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
-            .Should().Be(DateTime.ParseExact("05/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
-
-        [TestMethod]
-        public void test9()
-        {
-            // |
-            // |
-            // X 10:00:00
-            // |
-            // |
-            // - 12:00:00
-            // 
-            // 
-            // - 14:00:00
-            // |
-            // |
-
-            new Schedule
-            {
-                OpenDay = Wednesday,
-                OpenTime = TimeSpan.Parse("14:00:00"),
-
-                CloseDay = Wednesday,
-                CloseTime = TimeSpan.Parse("12:00:00")
-            }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
-            .Should().Be(DateTime.ParseExact("28/06/2017 12:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
-        }
-
-        [TestMethod]
-        public void test10()
-        {
-            // |
-            // |
-            // - 10:00:00
-            // 
-            // 
-            // - 12:00:00
-            // |
-            // |
-            // X 14:00:00
-            // |
-            // |
-
-            new Schedule
-            {
-                OpenDay = Wednesday,
-                OpenTime = TimeSpan.Parse("12:00:00"),
-
-                CloseDay = Wednesday,
-                CloseTime = TimeSpan.Parse("10:00:00")
-            }
-            .NextClosingTime(DateTime.ParseExact("28/06/2017 14:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null)) //Wednesday
-            .Should().Be(DateTime.ParseExact("05/07/2017 10:00:00.000", "dd/MM/yyyy HH:mm:ss.fff", null));
         }
     }
 }
