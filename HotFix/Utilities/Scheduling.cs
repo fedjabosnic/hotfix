6f8c8f94a7a96c22aab1bd61b36779e349caf453
George Flourentzos <georgeflourentzos@gmail.com>
2017-08-14 10:57:41 +0100

    Added comments (documentation) to the scheduling extension methods

--- HotFix/Utilities/Scheduling.cs
+++ HotFix/Utilities/Scheduling.cs
@@ -6,6 +6,12 @@ namespace HotFix.Utilities
 {
     public static class Scheduling
     {
+        /// <summary>
+        /// Returns the first session in the list that the given date matches, or null otherwise.
+        /// </summary>
+        /// <param name="sessions">A non-empty list of sessions.</param>
+        /// <param name="at">The date and time to check against the sessions.</param>
+        /// <returns>An active session, or null.</returns>
         public static ISchedule GetActive(this List<ISchedule> sessions, DateTime at)
         {
             foreach (var session in sessions)
@@ -16,6 +22,12 @@ namespace HotFix.Utilities
             return null;
         }
 
+        /// <summary>
+        /// Returns the closing datetime of the session if it is active, or the closing datetime of the next occurence of the session.
+        /// </summary>
+        /// <param name="schedule">The session whose closing time needs to be calculated.</param>
+        /// <param name="datetime">The date after which to calculate the closing time.</param>
+        /// <returns>The next closing date and time.</returns>
         public static DateTime NextClosingTime(this ISchedule schedule, DateTime datetime)
         {
             var startOfWeek = (datetime - TimeSpan.FromDays((int)datetime.DayOfWeek)).Date;
@@ -27,6 +39,12 @@ namespace HotFix.Utilities
             return datetime <= endDate ? endDate : endDate.AddDays(7);
         }
 
+        /// <summary>
+        /// Checks whether the datetime lies within the session.
+        /// </summary>
+        /// <param name="schedule">The session to check against.</param>
+        /// <param name="datetime">The date and time to check against.</param>
+        /// <returns>Whether the given datetime is in session.</returns>
         public static bool IsActiveAt(this ISchedule schedule, DateTime datetime)
         {
             var startOfWeek = (datetime - TimeSpan.FromDays((int)datetime.DayOfWeek)).Date;
